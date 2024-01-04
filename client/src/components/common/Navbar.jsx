7c003bb886031d2cbcbbb8c99398db34b20d55f8
rachel lea kohen <rkohen@g.jct.ac.il>
2024-01-04 18:51:40 +0200

    Good authorization and authentication

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -1,17 +1,36 @@
 import { Link } from "react-router-dom";
-function Navbar() {
-    return (
-        <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-            <Link className="navbar-brand" to="/home">Home</Link>
-            <Link className="navbar-brand" to="/login">Login</Link>
-            <Link className="navbar-brand" to="/register">Register</Link>
-            <Link className="navbar-brand" to="/profile">Profile</Link>
-            <Link className="navbar-brand" to="/logout">Logout</Link>
-        </nav>
-        </div>
-    );
-    
+import "./Navbar.css";
+const Navbar = ( isLoggedIn ) => {
+    const onLogout = () => {
+        localStorage.removeItem("token");
+        window.location.reload();
+    };
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="logo">My App</div>
+
+      {isLoggedIn ? (
+        <button onClick={onLogout}>Logout</button>
+      ) : (
+        <button>Login</button>
+      )}
+      <Link className="navbar-brand" to="/home">
+        Home
+      </Link>
+      <Link className="navbar-brand" to="/login">
+        Login
+      </Link>
+      <Link className="navbar-brand" to="/register">
+        Register
+      </Link>
+      <Link className="navbar-brand" to="/profile">
+        Profile
+      </Link>
+      <Link className="navbar-brand" to="/">
+        Logout
+      </Link>
+    </nav>
+  );
 }
 
 export default Navbar;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const bcrypt = require("bcryptjs");
-
-// Load middleware
 const cors = require("cors");
-const helmet = require("helmet");
 
 const app = express();
+
+var corsOptions = {
+  origin: "http://localhost:5173"
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -14,74 +16,132 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes
-require("../server/routes/auth.routes")(app);
-require("../server/routes/user.routes")(app);
+require('../server/routes/auth.routes')(app);
+require('../server/routes/user.routes')(app);
 
-// Load environment variables
-require("dotenv").config();
-const port = process.env.PORT;
-
-var corsOptions = {
-  origin: "http://localhost:8081",
-};
-app.use(cors(corsOptions));
-
-app.use(helmet());
-
-const db = require("./models");
-const { signin } = require("./controllers/auth.controller");
+const db = require("../server/models");
 const Role = db.role;
-const User = db.user;
 
-// basic user signup
-const admin = {
-  username: "admin",
-  email: "admin@test",
-  password: "0770",
-  roles: ["admin", "user"],
-};
-
-const mod = {
-  username: "mod",
-  email: "mod@test",
-  password: "0770",
-  roles: ["moderator", "user"],
-};
-
-const user = {
-  username: "user",
-  email: "user@test",
-  password: "0770",
-  roles: ["user"],
-};
-
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and Resync Db");
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
   initial();
 });
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "hello from express" });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
 
 function initial() {
   Role.create({
     id: 1,
-    name: "user",
+    name: "user"
   });
-
+ 
   Role.create({
     id: 2,
-    name: "moderator",
+    name: "moderator"
   });
-
+ 
   Role.create({
     id: 3,
-    name: "admin",
+    name: "admin"
   });
 }
 
-app.get("/", (req, res) => {
-  res.send("Hello from Express!");
-});
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server listening on port ${process.env.PORT}`);
-});
-app.listen();
+
+// const express = require("express");
+// const bcrypt = require("bcryptjs");
+
+// // Load middleware
+// const cors = require("cors");
+// const helmet = require("helmet");
+
+// const app = express();
+
+// // parse requests of content-type - application/json
+// app.use(express.json());
+
+// // parse requests of content-type - application/x-www-form-urlencoded
+// app.use(express.urlencoded({ extended: true }));
+
+// // routes
+// require("../server/routes/auth.routes")(app);
+// require("../server/routes/user.routes")(app);
+
+// // Load environment variables
+// require("dotenv").config();
+// const port = process.env.PORT;
+
+// var corsOptions = {
+//   origin: "http://localhost:8081",
+// };
+// app.use(cors(corsOptions));
+
+// app.use(helmet());
+
+// const db = require("./models");
+// const { signin } = require("./controllers/auth.controller");
+// const Role = db.role;
+// const User = db.user;
+
+// // basic user signup
+// const admin = {
+//   username: "admin",
+//   email: "admin@test",
+//   password: "0770",
+//   roles: ["admin", "user"],
+// };
+
+// const mod = {
+//   username: "mod",
+//   email: "mod@test",
+//   password: "0770",
+//   roles: ["moderator", "user"],
+// };
+
+// const user = {
+//   username: "user",
+//   email: "user@test",
+//   password: "0770",
+//   roles: ["user"],
+// };
+
+// db.sequelize.sync({ force: true }).then(() => {
+//   console.log("Drop and Resync Db");
+//   initial();
+// });
+
+// function initial() {
+//   Role.create({
+//     id: 1,
+//     name: "user",
+//   });
+
+//   Role.create({
+//     id: 2,
+//     name: "moderator",
+//   });
+
+//   Role.create({
+//     id: 3,
+//     name: "admin",
+//   });
+// }
+
+// app.get("/", (req, res) => {
+//   res.send("Hello from Express!");
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server listening on port ${port}`);
+// });
+// app.listen();
