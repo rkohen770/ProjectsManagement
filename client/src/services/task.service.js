8519a1202c3cb5ec09e58e5a1ecd0e6342b29de1
rachel lea kohen <rkohen@g.jct.ac.il>
2024-01-05 10:58:23 +0200

    table with sequelize

---
+++ client/src/services/task.service.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'http://localhost:8080/api/tasks/';
+
+class TaskService {
+  getAllTasks() {
+    return axios.get(API_URL, { headers: authHeader() });
+  }
+
+  getTaskById(id) {
+    return axios.get(API_URL + id, { headers: authHeader() });
+  }
+
+  createTask(data) {
+    return axios.post(API_URL, data, { headers: authHeader() });
+  }
+
+  updateTask(id, data) {
+    return axios.put(API_URL + id, data, { headers: authHeader() });
+  }
+
+  deleteTask(id) {
+    return axios.delete(API_URL + id, { headers: authHeader() });
+  }
+}
+
+export default new TaskService();
--- server/database.sql
+++
@@ -1,5 +0,0 @@
-insert into user (username,email,password,roles)
-VALUES
-('admin','admin@localhost','admin','ROLE_ADMIN'),
-('user','user@localhost','user','ROLE_USER'),
-('user2','user2@localhost','user2','ROLE_USER')
